d66efc70dc7b0c2f16b0d7fa3af2fb1e384e0b4a
sokol.matija <sokol.matija@gmail.com>
2025-05-09 17:54:47 +0200

    SCRUM-89: Install and configure navigation package

---
+++ App.js
@@ -0,0 +1,11 @@
+import 'react-native-gesture-handler';
+import React from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+
+export default function App() {
+  return (
+    <NavigationContainer>
+      {/* Your app content will go here */}
+    </NavigationContainer>
+  );
+} 
